9e55e8957623e0cafe8384a0cb324c35b88af4bd
andres <andresnarvaez304@gmail.com>
2022-06-13 07:27:40 -0500

    implementando integracion del usuario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { ContainerFormItems, InputForm, TitleForm } from '../style/Form.element'
-import Button from "../components/Button"
+
+// import appAndresTech from "../credenciales";
+import {getFirestore, collection,addDoc, getDocs, doc, deleteDoc, getDoc, setDoc} from "firebase/firestore"
+import app from "../lib/firebase"
+
+const db = getFirestore(app);
+
 function Form() {
-  return (
-    <>
-        <ContainerFormItems>
-            <TitleForm>Formulario</TitleForm>
-            <InputForm placeholder='Nombre Completo' type="text"/>
-            <InputForm placeholder='Email Completo' type="email"/>
-            {Button("#", "Enviar")}
-        </ContainerFormItems>
-    </>
-  )
+
+    const valorInicial = {
+        title : "",
+        email : ""
+    }
+    const [formData, setFormData] = useState(valorInicial);
+
+    const changeHandle = e => {
+        const {name, value} = e.target;
+        setFormData({...formData,[name] : value })
+    }
+
+    const handleSaveData = async (e) =>{
+        e.preventDefault();
+        try {
+            await addDoc(collection(db,"Usuarios"), {
+                ...formData
+            })
+        } catch (error) {
+            console.log("conexion fallida con la BD", error)
+        }
+        setFormData({...valorInicial})
+    }
+
+    
+    return (
+        <>
+            <ContainerFormItems onSubmit={handleSaveData}>
+                <TitleForm>Formulario</TitleForm>
+                <InputForm placeholder='Nombre Completo' name= "title" type="text" onChange={changeHandle} value={formData.title}/>
+                <InputForm placeholder='Email Completo'  name='email' type="email" onChange={changeHandle} value ={formData.email}/>
+                <InputForm type="submit" value="Enviar"/>
+            </ContainerFormItems>
+        </>
+    )
 }
 
 export default Form
---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgNaqG9zwlvD4hA8TecaP7uCzRsDkEhig",
+  authDomain: "andrestech-a6a49.firebaseapp.com",
+  projectId: "andrestech-a6a49",
+  storageBucket: "andrestech-a6a49.appspot.com",
+  messagingSenderId: "424892343948",
+  appId: "1:424892343948:web:b2041e22cb87754d243da9",
+  measurementId: "G-DMSSEFPGY4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
+
