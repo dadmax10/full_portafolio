32e7bd9c848d7ccf92984ce998faa00c001c8f73
andres <andresnarvaez304@gmail.com>
2022-06-16 08:52:18 -0500

    modificando boton

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,28 +1,15 @@
 import React from 'react'
 
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const LinkButton = styled.a`
-  width: auto;
-  display:inline-block;
-  padding:1em 4em;
-  background:transparent;
-  color:#08fdd8;
-  border: 2px solid #08fdd8;
-  text-decoration:none;
-  font-size:1.3em;
-  margin:1em 0px;
-  &:hover{
-    background:#08fdd8;
-    color:#000;
-    transition:1s;
-  }
-`
+import "../styles/styleButton.css"
+
+
 
 function Button(linkBoton, textBoton) {
   return (
     <>
-      <LinkButton href={linkBoton}>{textBoton}</LinkButton>
+      <a className="linkButton" style={{"--clr" : "#08fdd9" }} href={linkBoton}><span>{textBoton}</span><i></i></a>
     </>
   )
 }
